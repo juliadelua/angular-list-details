aadbdca6a51eb60cdf84f18e862b0ad977714955
juliadelua <juliagatoh@gmail.com>
2022-03-11 20:47:39 -0300

    7 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,4 +1,4 @@
-export const listData = [
+export const listExample = [
   {
     id: 1,
     text: 'Text Um',
@@ -10,5 +10,57 @@ export const listData = [
   {
     id: 3,
     text: 'Texto Três',
+  },
+];
+
+export const listData = [
+  {
+    id: 'ISW014',
+    text: 'Projeto de Encontrabilidade',
+    day: 'Segunda',
+    time: '19:00 - 22:30',
+    ementa:
+      'Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises.',
+    objetivos:
+      'Explorar estratégias que direcionam o tráfego para um site, ajudam os usuários a encontrar conteúdo dentro de um site e encorajam consultas de retorno. Discutir os Métodos de Otimização de Search Engine (SEO), estratégias de marketing, análise de tráfego e teoria em torno comportamento de busca. Investigar as relações entre usabilidade, acessibilidade, arquitetura da informação, marketing, e encontrabilidade, a fim de implementar estratégias para a criação de conteúdo encontrável. Objetivos específicos.  Criar template para o Blog sobre encontrabilidade, apresentar resultados das pesquisas sobre os temas (técnicas e ou utilitários SEO, técnicas e ou utilitários de marketing por e-mail, Microformatos, busca, teoria da encontrabilidade, marketing online). Criar um search engine amigável. Criar conteúdo encontrável aplicando técnicas de acessibilidade e os padrões Web. Implantar um sítio Internet com todos os recursos estudados até o semestre.',
+  },
+  {
+    id: 'AGM004',
+    text: 'Negócios e Marketing e Eletrônicos',
+    day: 'Quarta',
+    time: '19:00 - 22:30',
+    ementa:
+      'Conceitos de marketing e de marketing digital. O comportamento do consumidor on-line. Marketing de conteúdo. Marketing nas mídias sociais. E-mail marketing. Marketing viral. Publicidade on-line. Pesquisa de marketing on-line. Marketing e os negócios na Internet. Estudos de caso.',
+    objetivos:
+      'Apresentar e discutir as questões do marketing na era da Internet. Abordar as principais estratégias, ações, ferramentas e mídias presentes na Web. Objetivos específicos. Compreender e utilizar a Internet para os negócios e implementar ações de marketing, comunicação e publicidade on-line.',
+  },
+  {
+    id: 'IDS002',
+    text: 'Desenvolvimento para Servidores II',
+    day: 'Quinta',
+    time: '19:00 - 22:30',
+    ementa:
+      'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros).',
+    objetivos:
+      'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto. Objetivos específicos. Implementar softwares do lado servidor e com uso de uma linguagem de programação e de padrões de projetos mais usuais como MVC, DAO, Composite, Singleton, entre outros.',
+  },
+  {
+    id: 'ITE004',
+    text: 'Tópicos Especiais em Sistemas para Internet II',
+    day: 'Sexta',
+    time: '19:00 - 22:30',
+    ementa: 'Recursos e aplicações da tecnologia escolhida.',
+    objetivos:
+      'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado.',
+  },
+  {
+    id: 'ISW011',
+    text: 'Projeto de Prototipagem e Testes de Usabilidade',
+    day: 'Sábado',
+    time: '8:00 - 11:30',
+    ementa:
+      'Conceitos, métodos de prototipagem e ferramentas de teste de protótipos. Conceitos de usabilidade e estratégias de design. Métodos de teste de usabilidade. Planejamento de teste de usabilidade. Recrutamento. Projeto de usabilidade. Plano de teste. Sessão de teste de usabilidade. Análise de dados. Comunicação dos resultados. Variação de técnicas.',
+    objetivos:
+      'Aprofundar os conceitos de design da usabilidade  e métodos de ensaio para mídia interativa. Usabilidade baseada na adaptação de design de interface para uma experiência do usuário mais eficaz.  Apresentar e usar diferentes métodos e ferramentas de prototipagem para explorar com os colegas de equipe ou clientes: conceitos de design, comunicação e testes de usabilidade. Objetivos específicos. Os estudantes serão capazes de criar uma série de protótipos para aplicações Web hipotéticas Criar um protótipo interativo baseando-se no protótipo de papel. Revisar protótipo. Projetar, conduzir e analisar testes de usabilidade com base: nos princípios estabelecidos, nos resultados de pesquisa com o protótipo e na teoria.  Desenvolver um plano de teste de usabilidade, recrutar os usuários apropriados, criar cenários robustos de tarefas, facilitar os testes,  analisar e comunicar os resultados do teste.',
   },
 ];
